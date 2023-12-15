7d4efd64eb5cdad745545d7dbad5b2cb9cea03b2
Tomás Beckmann <147322170+tomasbeckmann@users.noreply.github.com>
2023-12-15 00:35:02 +0000

    finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,28 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useState } from "react";
 
 //create your first component
-const Home = () => {
+function Home() {
+
+const [ color, setColor] = useState("")
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="traffic-lights">
+			<button onClick={() => setColor("red")} className={color === "red" ? "red-selected" : "red"}></button>
+			<button onClick={() => setColor("yellow")} className={color === "yellow" ? "yellow-selected" : "yellow"}></button>
+			<button onClick= {() => setColor("green")} className={color=== "green" ? "green-selected" : "green"}></button>
 		</div>
 	);
 };
 
 export default Home;
+
+// onClick={() => setColor("")}
+
+/* actualizar el valor de usestate "red" en color
+	<button onClick={() => setColor("red")} className={class}></button>
+	reemplazar valor color en variable:
+	function class (color){
+		return color + -selected
+	}
+	 */
